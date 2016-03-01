516042dcfd2fbe62de7000e04a4d8e203700ad29
ruche7 <pg@ruche-home.net>
2016-03-02 02:20:43 +0900

    スレッドID付きデバッグ出力クラス追加。

---
+++ Util/ThreadDebug.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace RucheHome.Util
+{
+    /// <summary>
+    /// スレッドID付きのデバッグ出力を行う静的クラス。
+    /// </summary>
+    public static class ThreadDebug
+    {
+        /// <summary>
+        /// メッセージと改行をデバッグ出力する。
+        /// </summary>
+        /// <param name="message">メッセージ。</param>
+        [Conditional("DEBUG")]
+        public static void WriteLine(string message)
+        {
+            var tid = @"[TID:" + Thread.CurrentThread.ManagedThreadId + @"] ";
+            Debug.WriteLine(tid + message);
+        }
+
+        /// <summary>
+        /// 値と改行をデバッグ出力する。
+        /// </summary>
+        /// <param name="value">値。</param>
+        [Conditional("DEBUG")]
+        public static void WriteLine(object value)
+        {
+            WriteLine(value?.ToString() ?? @"(null)");
+        }
+
+        /// <summary>
+        /// フォーマット文字列と改行をデバッグ出力する。
+        /// </summary>
+        /// <param name="format">フォーマット。</param>
+        /// <param name="args">パラメータ配列。</param>
+        [Conditional("DEBUG")]
+        public static void WriteLine(string format, params object[] args)
+        {
+            WriteLine(string.Format(format, args));
+        }
+    }
+}
