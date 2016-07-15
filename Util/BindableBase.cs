494f994d02e36ac7bff4e72ace22c5eeda312091
ruche7 <pg@ruche-home.net>
2016-07-15 18:44:01 +0900

    BindableBase クラスを追加し、 BindableConfigBase クラスのベースクラスとした。

---
+++ Util/BindableBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RucheHome.Util
+{
+    /// <summary>
+    /// プロパティ変更通知をサポートするクラスの抽象基底クラス。
+    /// </summary>
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        protected BindableBase()
+        {
+        }
+
+        /// <summary>
+        /// プロパティ値を設定し、変更をイベント通知する。
+        /// </summary>
+        /// <typeparam name="T">プロパティ値の型。</typeparam>
+        /// <param name="field">設定先フィールド。</param>
+        /// <param name="value">設定値。</param>
+        /// <param name="propertyName">
+        /// プロパティ名。 CallerMemberNameAttribute により自動設定される。
+        /// </param>
+        protected void SetProperty<T>(
+            ref T field,
+            T value,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                this.RaisePropertyChanged(propertyName);
+            }
+        }
+
+        /// <summary>
+        /// PropertyChanged イベントを発生させる。
+        /// </summary>
+        /// <param name="propertyName">
+        /// プロパティ名。指定しなければ CallerMemberNameAttribute により自動設定される。
+        /// </param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #region INotifyPropertyChanged の実装
+
+        /// <summary>
+        /// プロパティ値の変更時に呼び出されるイベント。
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
--- Util/BindableConfigBase.cs
+++ Util/BindableConfigBase.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 
 namespace RucheHome.Util
@@ -13,42 +10,13 @@ namespace RucheHome.Util
     /// プロパティ変更通知付き設定の抽象基底クラス。
     /// </summary>
     [DataContract(Namespace = "")]
-    public abstract class BindableConfigBase
-        :
-        INotifyPropertyChanged,
-        IExtensibleDataObject
+    public abstract class BindableConfigBase : BindableBase, IExtensibleDataObject
     {
         /// <summary>
         /// コンストラクタ。
         /// </summary>
-        protected BindableConfigBase()
+        protected BindableConfigBase() : base()
         {
-        }
-
-        /// <summary>
-        /// プロパティ値を設定し、変更をイベント通知する。
-        /// </summary>
-        /// <typeparam name="T">プロパティ値の型。</typeparam>
-        /// <param name="field">設定先フィールド。</param>
-        /// <param name="value">設定値。</param>
-        /// <param name="propertyName">
-        /// プロパティ名。 CallerMemberNameAttribute により自動設定される。
-        /// </param>
-        protected void SetProperty<T>(
-            ref T field,
-            T value,
-            [CallerMemberName] string propertyName = null)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, value))
-            {
-                field = value;
-                if (propertyName != null && this.PropertyChanged != null)
-                {
-                    this.PropertyChanged(
-                        this,
-                        new PropertyChangedEventArgs(propertyName));
-                }
-            }
         }
 
         /// <summary>
@@ -93,15 +61,6 @@ namespace RucheHome.Util
                 fi.SetValue(this, fi.GetValue(src));
             }
         }
-
-        #region INotifyPropertyChanged の実装
-
-        /// <summary>
-        /// プロパティ値の変更時に呼び出されるイベント。
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
 
         #region IExtensibleDataObject の明示的実装
 
