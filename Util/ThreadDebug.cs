00da312d9238170cdd14c344ce3c1f9128faa32d
ruche7 <pg@ruche-home.net>
2016-07-17 10:37:06 +0900

    WriteException メソッド追加。

--- Util/ThreadDebug.cs
+++ Util/ThreadDebug.cs
@@ -59,6 +59,37 @@ namespace RucheHome.Util
         }
 
         /// <summary>
+        /// 時刻、スレッドID、呼び出し元情報を付けて例外情報と改行をデバッグ出力する。
+        /// </summary>
+        /// <param name="exception">例外情報。</param>
+        /// <param name="withStackTrace">
+        /// スタックトレースを出力するならば true 。そうでなければ false 。
+        /// </param>
+        /// <param name="member">
+        /// 呼び出し元メンバ名。 CallerMemberNameAttribute により自動設定される。
+        /// </param>
+        /// <param name="file">
+        /// 呼び出し元ファイル名。 CallerFilePathAttribute により自動設定される。
+        /// </param>
+        /// <param name="line">
+        /// 呼び出し元行番号。 CallerLineNumberAttribute により自動設定される。
+        /// </param>
+        [Conditional("DEBUG")]
+        public static void WriteException(
+            Exception exception,
+            bool withStackTrace = false,
+            [CallerMemberName] string member = "",
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            WriteLine(
+                MakeExceptionText(exception, withStackTrace),
+                member,
+                file,
+                line);
+        }
+
+        /// <summary>
         /// デバッグ出力用メッセージを作成する。
         /// </summary>
         /// <param name="text">テキスト。不要ならば null 。</param>
@@ -93,6 +124,30 @@ namespace RucheHome.Util
             }
 
             return msg.ToString();
+        }
+
+        /// <summary>
+        /// 例外情報テキストを作成する。
+        /// </summary>
+        /// <param name="exception">例外情報。</param>
+        /// <param name="withStackTrace">
+        /// スタックトレースを含めるならば true 。そうでなければ false 。
+        /// </param>
+        /// <returns>例外情報テキスト。</returns>
+        internal static string MakeExceptionText(Exception exception, bool withStackTrace)
+        {
+            if (exception == null)
+            {
+                return null;
+            }
+
+            var text = exception.GetType().FullName + @": " + exception.Message;
+            if (withStackTrace)
+            {
+                text += Environment.NewLine + exception.StackTrace;
+            }
+
+            return text;
         }
     }
 }
--- Util/ThreadTrace.cs
+++ Util/ThreadTrace.cs
@@ -54,5 +54,36 @@ namespace RucheHome.Util
         {
             WriteLine(value?.ToString(), member, file, line);
         }
+
+        /// <summary>
+        /// 時刻、スレッドID、呼び出し元情報を付けて例外情報と改行をトレース出力する。
+        /// </summary>
+        /// <param name="exception">例外情報。</param>
+        /// <param name="withStackTrace">
+        /// スタックトレースを出力するならば true 。そうでなければ false 。
+        /// </param>
+        /// <param name="member">
+        /// 呼び出し元メンバ名。 CallerMemberNameAttribute により自動設定される。
+        /// </param>
+        /// <param name="file">
+        /// 呼び出し元ファイル名。 CallerFilePathAttribute により自動設定される。
+        /// </param>
+        /// <param name="line">
+        /// 呼び出し元行番号。 CallerLineNumberAttribute により自動設定される。
+        /// </param>
+        [Conditional("TRACE")]
+        public static void WriteException(
+            Exception exception,
+            bool withStackTrace = false,
+            [CallerMemberName] string member = "",
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            WriteLine(
+                ThreadDebug.MakeExceptionText(exception, withStackTrace),
+                member,
+                file,
+                line);
+        }
     }
 }
