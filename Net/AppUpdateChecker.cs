97f281c10b8562d25c0ac2587075dfc94786ca0a
ruche7 <pg@ruche-home.net>
2016-11-05 16:18:35 +0900

    AppUpdateChecker クラス更新
    - 新JSON項目追加。
    - WebClient が Dispose されていなかったので修正。

--- Net/AppUpdateChecker.cs
+++ Net/AppUpdateChecker.cs
@@ -14,16 +14,75 @@ namespace RucheHome.Net
     /// <summary>
     /// アプリ更新情報チェッククラス。
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// アプリ更新情報JSONファイルには、下記の項目を持つオブジェクトを定義すること。
+    /// </para>
+    /// <list type="table">
+    /// <listheader>
+    /// <term>項目</term>
+    /// <description>値</description>
+    /// </listheader>
+    /// <item>
+    /// <term>product</term>
+    /// <description>
+    /// プロダクト名を定義する。リダイレクトしないならば定義必須。
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>version</term>
+    /// <description>
+    /// バージョンを定義する。リダイレクトしないならば定義必須。
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>page_uri</term>
+    /// <description>
+    /// ダウンロードページURIを定義する。リダイレクトしないならば定義必須。
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>display_name</term>
+    /// <description>
+    /// 表示名を定義する。定義が無ければ product および version が利用される。
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>redirect_base_uri</term>
+    /// <description>
+    /// リダイレクト用のベースURIを定義する。
+    /// redirect_product とどちらか片方もしくは両方が定義されていればリダイレクトする。
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>redirect_product</term>
+    /// <description>
+    /// リダイレクト用のプロダクト名を定義する。
+    /// redirect_base_uri とどちらか片方もしくは両方が定義されていればリダイレクトする。
+    /// </description>
+    /// </item>
+    /// </list> 
+    /// </remarks>
     public class AppUpdateChecker : BindableBase
     {
         /// <summary>
+        /// アプリ更新情報JSONファイルの既定ベースURI。
+        /// </summary>
+        public static readonly string DefaultBaseUri =
+            @"https://ruche-home.net/files/versions/";
+
+        /// <summary>
         /// 現在実行中のプロセスを用いるコンストラクタ。
         /// </summary>
+        /// <param name="baseUri">
+        /// アプリ更新情報JSONファイルのベースURI。既定のURIを用いるならば null 。
+        /// </param>
         /// <remarks>
         /// 実行中のプロセスに AssemblyProductAttribute 属性と
         /// AssemblyVersionAttribute 属性が定義されている必要がある。
         /// </remarks>
-        public AppUpdateChecker() : this(Assembly.GetEntryAssembly())
+        public AppUpdateChecker(string baseUri = null)
+            : this(Assembly.GetEntryAssembly(), baseUri)
         {
         }
 
@@ -35,7 +94,10 @@ namespace RucheHome.Net
         /// AssemblyProductAttribute 属性と
         /// AssemblyVersionAttribute 属性が定義されている必要がある。
         /// </param>
-        public AppUpdateChecker(Assembly app)
+        /// <param name="baseUri">
+        /// アプリ更新情報JSONファイルのベースURI。既定のURIを用いるならば null 。
+        /// </param>
+        public AppUpdateChecker(Assembly app, string baseUri = null)
         {
             if (app == null)
             {
@@ -64,43 +126,61 @@ namespace RucheHome.Net
                     nameof(app));
             }
 
-            this.Product = productAttr.Product;
+            this.CurrentProduct = productAttr.Product;
             this.CurrentVersion = version;
+            this.BaseUri = baseUri ?? DefaultBaseUri;
         }
 
         /// <summary>
         /// コンストラクタ。
         /// </summary>
-        /// <param name="product">プロダクト名。</param>
+        /// <param name="currentProduct">現行プロダクト名。</param>
         /// <param name="currentVersion">現行バージョン。</param>
-        public AppUpdateChecker(string product, Version currentVersion)
+        /// <param name="baseUri">
+        /// アプリ更新情報JSONファイルのベースURI。既定のURIを用いるならば null 。
+        /// </param>
+        public AppUpdateChecker(
+            string currentProduct,
+            Version currentVersion,
+            string baseUri = null)
         {
-            if (product == null)
+            if (currentProduct == null)
             {
-                throw new ArgumentNullException(nameof(product));
+                throw new ArgumentNullException(nameof(currentProduct));
             }
-            if (string.IsNullOrWhiteSpace(product))
+            if (string.IsNullOrWhiteSpace(currentProduct))
             {
-                throw new ArgumentException(@"`product` is blank.", nameof(product));
+                throw new ArgumentException(
+                    @"`product` is blank.",
+                    nameof(currentProduct));
             }
             if (currentVersion == null)
             {
                 throw new ArgumentNullException(nameof(currentVersion));
             }
 
-            this.Product = product;
+            this.CurrentProduct = currentProduct;
             this.CurrentVersion = currentVersion;
+            this.BaseUri = baseUri ?? DefaultBaseUri;
         }
 
         /// <summary>
-        /// プロダクト名を取得する。
+        /// 現行プロダクト名を取得する。
         /// </summary>
-        public string Product { get; }
+        public string CurrentProduct { get; }
 
         /// <summary>
         /// 現行バージョンを取得する。
         /// </summary>
         public Version CurrentVersion { get; }
+
+        /// <summary>
+        /// アプリ更新情報JSONファイルのベースURIを取得する。
+        /// </summary>
+        /// <remarks>
+        /// このURIの末尾に プロダクト名 + ".json" を付けたURIへアクセスする。
+        /// </remarks>
+        public string BaseUri { get; }
 
         /// <summary>
         /// 更新チェック処理中であるか否かを取得する。
@@ -124,6 +204,19 @@ namespace RucheHome.Net
             private set { this.SetProperty(ref this.canUpdate, value); }
         }
         private bool canUpdate = false;
+
+        /// <summary>
+        /// 最新プロダクト名を取得する。
+        /// </summary>
+        /// <remarks>
+        /// 未チェック状態では null を返す。
+        /// </remarks>
+        public string NewestProduct
+        {
+            get { return this.newestProduct; }
+            private set { this.SetProperty(ref this.newestProduct, value); }
+        }
+        private string newestProduct = null;
 
         /// <summary>
         /// 最新バージョンを取得する。
@@ -152,6 +245,19 @@ namespace RucheHome.Net
         private Uri pageUri = null;
 
         /// <summary>
+        /// 表示名を取得する。
+        /// </summary>
+        /// <remarks>
+        /// 未チェック状態では null を返す。
+        /// </remarks>
+        public string DisplayName
+        {
+            get { return this.displayName; }
+            private set { this.SetProperty(ref this.displayName, value); }
+        }
+        private string displayName = null;
+
+        /// <summary>
         /// 更新チェック処理を行う。
         /// </summary>
         /// <returns>成功したならば true 。そうでなければ false 。</returns>
@@ -162,52 +268,33 @@ namespace RucheHome.Net
                 return false;
             }
 
-            Version version = null;
-            Uri pageUri = null;
-
             try
             {
                 this.IsBusy = true;
 
-                // アプリ更新情報JSONファイルをダウンロード
-                var json =
-                    await this.Client.DownloadDataTaskAsync(
-                        JsonBaseUri + this.Product + @".json");
-                if (json == null || json.Length <= 0)
+                AppInfo info = null;
+                using (var webClient = new WebClient())
                 {
-                    return false;
+                    // キャッシュしない
+                    webClient.CachePolicy =
+                        new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+
+                    info =
+                        await DownloadAppInfo(
+                            webClient,
+                            this.BaseUri,
+                            this.CurrentProduct,
+                            this.CurrentProduct,
+                            this.CurrentVersion);
                 }
 
-                // JSONを読み取る
-                XElement elem = null;
-                using (
-                    var reader =
-                        JsonReaderWriterFactory.CreateJsonReader(
-                            json,
-                            XmlDictionaryReaderQuotas.Max))
-                {
-                    elem = await Task.Run(() => XElement.Load(reader));
-                }
+                this.NewestProduct = info.Product;
+                this.NewestVersion = info.Version;
+                this.PageUri = info.PageUri;
+                this.DisplayName = info.DisplayName;
 
-                var productElem = elem.Element(@"product");
-                var versionElem = elem.Element(@"version");
-                var pageUriElem = elem.Element(@"page_uri");
-
-                if (
-                    productElem == null ||
-                    versionElem == null ||
-                    pageUriElem == null ||
-                    (string)productElem != this.Product)
-                {
-                    return false;
-                }
-
-                version = new Version((string)versionElem);
-                pageUri = new Uri((string)pageUriElem);
-
-                this.NewestVersion = version;
-                this.PageUri = pageUri;
-                this.CanUpdate = (this.NewestVersion > this.CurrentVersion);
+                // CanUpdate は最後に更新する
+                this.CanUpdate = info.CanUpdate;
             }
             catch
             {
@@ -224,19 +311,165 @@ namespace RucheHome.Net
         private int runLock = 0;
 
         /// <summary>
-        /// アプリ更新情報JSONファイルのベースURI。
+        /// アプリ情報クラス。
         /// </summary>
-        private static readonly string JsonBaseUri =
-            @"https://ruche-home.net/files/versions/";
+        private class AppInfo
+        {
+            /// <summary>
+            /// プロダクト名を取得または設定する。
+            /// </summary>
+            public string Product { get; set; } = null;
+
+            /// <summary>
+            /// バージョンを取得または設定する。
+            /// </summary>
+            public Version Version { get; set; } = null;
+
+            /// <summary>
+            /// ダウンロードページURIを取得または設定する。
+            /// </summary>
+            public Uri PageUri { get; set; } = null;
+
+            /// <summary>
+            /// 表示名を取得または設定する。
+            /// </summary>
+            public string DisplayName { get; set; } = null;
+
+            /// <summary>
+            /// 新しいバージョンのアプリが存在するか否かを取得または設定する。
+            /// </summary>
+            public bool CanUpdate { get; set; } = false;
+        }
 
         /// <summary>
-        /// Webクライアントを取得する。
+        /// リダイレクトの最大許容回数。
         /// </summary>
-        private WebClient Client { get; } =
-            new WebClient
+        private const int RedirectCountLimit = 3;
+
+        /// <summary>
+        /// アプリ情報をダウンロードする。
+        /// </summary>
+        /// <param name="webClient">Webクライアント。</param>
+        /// <param name="baseUri">ベースURI。</param>
+        /// <param name="product">JSONファイル名に用いられるプロダクト名。</param>
+        /// <param name="currentProduct">現行プロダクト名。</param>
+        /// <param name="currentVersion">現行バージョン。</param>
+        /// <param name="redirectCount">リダイレクト回数。</param>
+        /// <returns>アプリ情報。ダウンロードできなかった場合は null 。</returns>
+        private static async Task<AppInfo> DownloadAppInfo(
+            WebClient webClient,
+            string baseUri,
+            string product,
+            string currentProduct,
+            Version currentVersion,
+            int redirectCount = 0)
+        {
+            if (webClient == null)
             {
-                // キャッシュしない
-                CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore)
-            };
+                throw new ArgumentNullException(nameof(webClient));
+            }
+            if (baseUri == null)
+            {
+                throw new ArgumentNullException(nameof(baseUri));
+            }
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+            if (currentProduct == null)
+            {
+                throw new ArgumentNullException(nameof(currentProduct));
+            }
+            if (currentVersion == null)
+            {
+                throw new ArgumentNullException(nameof(currentVersion));
+            }
+
+            // リダイレクト回数制限オーバー
+            if (redirectCount > RedirectCountLimit)
+            {
+                return null;
+            }
+
+            // アプリ更新情報JSONファイルをダウンロード
+            var json =
+                await webClient.DownloadDataTaskAsync(baseUri + product + @".json");
+            if (json == null || json.Length <= 0)
+            {
+                return null;
+            }
+
+            // JSONを読み取る
+            XElement elem = null;
+            using (
+                var reader =
+                    JsonReaderWriterFactory.CreateJsonReader(
+                        json,
+                        XmlDictionaryReaderQuotas.Max))
+            {
+                elem = await Task.Run(() => XElement.Load(reader));
+            }
+
+            // リダイレクト情報確認
+            var redirectBaseUriElem = elem.Element(@"redirect_base_uri");
+            var redirectProductElem = elem.Element(@"redirect_product");
+            if (redirectBaseUriElem != null || redirectProductElem != null)
+            {
+                // リダイレクトする
+                return
+                    await DownloadAppInfo(
+                        webClient,
+                        (redirectBaseUriElem == null) ?
+                            baseUri : (string)redirectBaseUriElem,
+                        (redirectProductElem == null) ?
+                            product : (string)redirectProductElem,
+                        currentProduct,
+                        currentVersion,
+                        ++redirectCount);
+            }
+
+            // 情報取得
+            var productElem = elem.Element(@"product");
+            var versionElem = elem.Element(@"version");
+            var pageUriElem = elem.Element(@"page_uri");
+            var displayNameElem = elem.Element(@"display_name");
+
+            if (productElem == null || versionElem == null || pageUriElem == null)
+            {
+                return null;
+            }
+
+            var newProduct = (string)productElem;
+            var newVersion = new Version((string)versionElem);
+            var pageUri = new Uri((string)pageUriElem);
+
+            // 表示名作成
+            string displayName;
+            if (displayNameElem == null)
+            {
+                // プロダクト名は変化している時のみ付ける
+                displayName =
+                    ((newProduct == currentProduct) ? "" : (newProduct + @" ")) +
+                    @"version " + newVersion;
+            }
+            else
+            {
+                displayName = (string)displayNameElem;
+            }
+
+            // プロダクト名が変化している場合も新バージョン扱い
+            bool canUpdate =
+                (newVersion > currentVersion || newProduct != currentProduct);
+
+            return
+                new AppInfo
+                {
+                    Product = newProduct,
+                    Version = newVersion,
+                    PageUri = pageUri,
+                    DisplayName = displayName,
+                    CanUpdate = canUpdate,
+                };
+        }
     }
 }
